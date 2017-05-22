3b8793a62473b8f890bff16da9e2d03f8b1378ee
Kristobal <junta.kristobal@gmail.com>
2017-05-22 23:38:25 +0300

    quotes change

--- sql-autobackup/sqlbackup.py
+++ sql-autobackup/sqlbackup.py
@@ -11,7 +11,7 @@ user = config.get('credentials', 'user')
 password = config.get('credentials', 'password')
 backupPath = config.get('backup_dir', 'path')
 
-databasesRaw = subprocess.check_output('mysql -u{} -p{} -e \'show databases;\''.format(user, password), shell=True)
+databasesRaw = subprocess.check_output("mysql -u{} -p{} -e 'show databases;'".format(user, password), shell=True)
 
 databases = databasesRaw.split()
 excludeDb = ['Database', 'mysql', 'phpmyadmin', 'information_schema', 'performance_schema']
